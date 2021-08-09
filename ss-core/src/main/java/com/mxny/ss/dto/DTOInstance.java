e86b4dc55525f43f637af69f8c4876d513caeccc
wangmi <wangmi@diligrp.com>
2021-08-09 11:32:51 +0800

    4.0.1 src ss-core: 创建DTO性能优化，添加DTOInstance

--- ss-core/src/main/java/com/mxny/ss/dto/DTOInstance.java
+++ ss-core/src/main/java/com/mxny/ss/dto/DTOInstance.java
@@ -7,7 +7,8 @@ import java.util.Map;
  * DTO实例缓存
  */
 public class DTOInstance {
-    public static final Map<Class<? extends IDTO>, Class<? extends IDTO>> cache = new HashMap<>(20);
+    public static final Map<Class<? extends IDTO>, Class<? extends IDTO>> CLASS_CACHE = new HashMap<>(32);
+    public static final Map<Class<? extends IDTO>, IDTO> INSTANCE_CACHE = new HashMap<>(32);
     public static final String SUFFIX = "$Impl";
     //根据DTOScan注解是否扫描到了IDTO接口，判断在框架中是否主力使用DTOInstance
     public static Boolean useInstance = false;
