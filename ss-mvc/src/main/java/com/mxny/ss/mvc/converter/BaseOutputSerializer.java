cce2f294f529f855a1cccbe0b707cbea4f5af6d4
wangmi <wangmi@diligrp.com>
2021-10-27 15:17:34 +0800

    4.1.0 feat(ss-mvc): add BaseOutputSerializer

---
+++ ss-mvc/src/main/java/com/mxny/ss/mvc/converter/BaseOutputSerializer.java
@@ -0,0 +1,34 @@
+package com.mxny.ss.mvc.converter;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.JSONSerializer;
+import com.alibaba.fastjson.serializer.ObjectSerializer;
+import com.alibaba.fastjson.serializer.SerializeWriter;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.mxny.ss.domain.BaseOutput;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+/**
+ * @author: WM
+ * @time: 2021/10/27
+ */
+public class BaseOutputSerializer implements ObjectSerializer {
+    public static final BaseOutputSerializer instance = new BaseOutputSerializer();
+    private static final String defaultPattern = "yyyy-MM-dd";
+
+    public BaseOutputSerializer() {
+    }
+
+    @Override
+    public void write(JSONSerializer serializer, Object object, Object fieldName, Type fieldType, int features) throws IOException {
+        SerializeWriter out = serializer.out;
+        if (object == null) {
+            out.writeNull();
+        } else {
+            BaseOutput result = (BaseOutput) object;
+            out.writeString(JSON.toJSONString(result, SerializerFeature.WRITE_MAP_NULL_FEATURES, SerializerFeature.WriteDateUseDateFormat , SerializerFeature.IgnoreErrorGetter));
+        }
+    }
+}
--- ss-mvc/src/main/java/com/mxny/ss/mvc/converter/JsonHttpMessageConverter.java
+++ ss-mvc/src/main/java/com/mxny/ss/mvc/converter/JsonHttpMessageConverter.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SimpleDateFormatSerializer;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.mxny.ss.domain.BaseOutput;
 import org.springframework.http.HttpOutputMessage;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 
@@ -30,6 +31,7 @@ public class JsonHttpMessageConverter extends FastJsonHttpMessageConverter{
         mapping.put(Timestamp.class, new SimpleDateFormatSerializer(dateFormat));
         mapping.put(LocalDateTime.class, LocalDateTimeSerializer.instance);
         mapping.put(LocalDate.class, LocalDateSerializer.instance);
+        mapping.put(BaseOutput.class, BaseOutputSerializer.instance);
     }
 
     @Override
